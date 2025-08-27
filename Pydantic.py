fe576074741b9abbd2997e1ab0d9303470d545fd
chironjit45 <chironjtbanerjee7492@gmail.com>
2025-08-27 10:02:19 +0600

    Pydantic.py with few change

--- Pydantic.py
+++ Pydantic.py
@@ -1,21 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import List, Dict, Optional
 
 
 class patient(BaseModel):
     name: str
+    email: EmailStr
     age: int
     weight: float
     married: bool = False
     allergies:Optional[list[str]] = None
-    contact_details: Dict[str, str]
+    phone_num: Dict[str, str]
 
 def insert_patient_data(patient: patient):
     print("Patient Information")
     print(f"Name: ", patient.name)
     print(f"Age:", patient.age)
     print(f"Weight: ", patient.weight)
-    print(f"Contact Details: ", patient.contact_details)
+    print(f"Phone: ", patient.contact_details)
     print('inserted')
 
 def update_patient_data(patient: patient):
@@ -23,10 +24,11 @@ def update_patient_data(patient: patient):
     print(f"Name: ",patient.name)
     print(f"Age:",patient.age)
     print(f"Weight: ",patient.weight)
-    print(f"Contact Details: ", patient.contact_details)
+    print(f"Patient Email: ",patient.email)
+    print(f"Contact Details: ", patient.phone_num)
     print("update")
 
-patient_info = {'name':'Hiro','age':30 ,'weight': 74.2,
-                'contact_details':{'email':'abs@gmail.com','phone':'19388494'} }
+patient_info = {'name':'Hiro','email':'abcd@gmail.com','age':30 ,'weight': 74.2,
+                'phone_num':{'phone':'19388494'}}
 patient1 = patient(**patient_info)
 update_patient_data(patient1)
